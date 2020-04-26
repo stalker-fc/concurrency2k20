eab231c3946dceacd16666734801cc141326e113
Sergey Stepanov <stepanov.s@numdes.com>
2020-04-26 11:40:22 +0300

    update matrix_generator

--- task_1/matrix_generator.cpp
+++ task_1/matrix_generator.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <cstdlib>
-using namespace std;
+#include <ctime>
 
-int main()
-{
-    cout << "Hello, world!" << endl;
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " " << "N_COLUMNS N_ROWS" << std::endl;
+        std::exit(1);
+    }
+
+    int n_columns = atoi(argv[1]);
+    int n_rows = atoi(argv[2]);
+
+    std::srand(std::time(NULL));
+    for (int i = 0; i < n_rows; i++) {
+        for (int j = 0; j < n_columns; j++) {
+            std::cout << std::rand() % n_columns << " ";
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
