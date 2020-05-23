6e870505e842646623732081e227e2084ceba93f
Sergey Stepanov <stepanov.s@numdes.com>
2020-05-23 21:21:40 +0300

    add sequential quick sort

--- implementation/task_3/sources/quick_sort_mpi.cpp
+++ implementation/task_3/sources/quick_sort_mpi.cpp
@@ -15,25 +15,56 @@ struct Array {
     int* data;
 };
 
+int get_partition(int* array, int left_index, int right_index) {
+    int pivot = array[left_index];
+    int i = left_index - 1;
+    int j = right_index + 1;
+    while (true) {
+		do {
+			i++;
+		} while (array[i] < pivot);
+		do {
+			j--;
+		} while (array[j] > pivot);
+
+        if (i >= j) {
+			return j;
+		}
+		array[i] += array[j];
+		array[j] = array[i] - array[j];
+		array[i] -= array[j];
+	}
+
+    return 0;
+}
+
+void quick_sort(int* array, int left_index, int right_index) {
+    if (left_index < right_index)
+	{
+		int pivot = get_partition(array, left_index, right_index);
+		quick_sort(array, left_index, pivot);
+		quick_sort(array, pivot + 1, right_index);
+	}
+}
 
 
-//int* quick_sort_mpi(int* array) {
+//int* quick_sort_mpi(int* array, int array_length) {
+//    int local_array_length = array_length / procs_count;
+//
 //}
 
 
 
-bool is_result_correct(int *sorted_array) {
-	bool is_correct = true;
-	std::size_t n_elems = sizeof(sorted_array) / sizeof(*sorted_array);
+bool is_result_correct(int *sorted_array, int array_length) {
+    for (std::size_t i = 0; i < array_length; ++i)
+	    std::cout << sorted_array[i] << ' ';
 
-	for (std::size_t i = 1; i < n_elems; ++i) {
-        if (sorted_array[i] > sorted_array[i - 1]) {
-			is_correct = false;
-			break;
-		}
+	for (std::size_t i = 1; i < array_length; ++i) {
+        if (sorted_array[i] < sorted_array[i - 1])
+			return false;
 	}
 
-	return is_correct;
+	return true;
 }
 
 Array get_array(char* filename) {
@@ -100,11 +131,11 @@ int main(int argc, char *argv[]) {
     MPI_Get_processor_name(processor_name, &namelen);
 
     auto start_time = std::chrono::steady_clock::now();
-    int *solution = quick_sort_mpi();
+    quick_sort(array.data, 0, array.length - 1);
     auto end_time = std::chrono::steady_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() << " ms" << std::endl;
 
-    bool is_correct = is_result_correct(solution);
+    bool is_correct = is_result_correct(array.data, array.length);
     if (!is_correct) {
         std::cerr << "Array has sorted incorrectly." << std::endl;
         exit(1);
