755cda15607449c4ced354fc6b9af4f1e5d34c4b
Sergey Stepanov <stepanov.s@numdes.com>
2020-06-03 15:34:20 +0300

    Update task_1 report

--- implementation/task_1/scripts/run.py
+++ implementation/task_1/scripts/run.py
@@ -3,18 +3,21 @@ import time
 from dataclasses import dataclass
 from pathlib import Path
 from typing import List
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 EXECUTABLE_FILE = Path(__file__).parent.parent / 'build' / 'main'
-
 
 TEST_CASES = {
     0: (100, 100),
     1: (250, 250),
     2: (500, 500),
     3: (1000, 1000),
-    # 4: (1500, 1500),
+    4: (200, 300),
+    5: (600, 800),
+    6: (1, 1000),
+    7: (1, 10000),
 }
 MODES = [1, 2]
 
@@ -37,7 +40,7 @@ def benchmark():
     benchmark_results = []
     for test_case, (n_rows, n_columns) in TEST_CASES.items():
         generate_random_matrix(n_rows, n_columns, 'A.txt')
-        generate_random_matrix(n_rows, n_columns, 'B.txt')
+        generate_random_matrix(n_columns, n_rows, 'B.txt')
         for mode in MODES:
             st = time.time()
             subprocess.call([EXECUTABLE_FILE, 'A.txt', 'B.txt', str(mode)])
@@ -57,8 +60,8 @@ def plot_results(benchmark_results: List[BenchmarkResult]):
         2: '#008ECC'
     }
     mode_descriptions = {
-        1: "sequential matrix multiplication",
-        2: "matrix multiplication using OpenMP"
+        1: "последовательное умножение матриц",
+        2: "умножение матриц с использованием OpenMP"
     }
     plt.figure(figsize=(4, 4))
     left, width = 0.1, 0.85
@@ -89,6 +92,12 @@ def plot_results(benchmark_results: List[BenchmarkResult]):
         execution_times = [res.execution_time_sec for res in group]
         test_cases = [res.test_case for res in group]
         ax.plot(test_cases, execution_times, marker='o', color=mode_colors[mode])
+        print(f'\n\n### {mode} {mode_descriptions[mode]}')
+        for res in group:
+            print(f'- Test Case №{res.test_case} '
+                  f'[{TEST_CASES[res.test_case][0]} x {TEST_CASES[res.test_case][1]}] * '
+                  f'[{TEST_CASES[res.test_case][1]} x {TEST_CASES[res.test_case][0]}]: '
+                  f'{res.execution_time_sec} sec')
 
     plt.savefig('task_1_report.png', format='png', dpi=200)
 
