d7bc69f0a36125d2b5fd4b62429598ec03712718
Sergey Stepanov <stepanov.s@numdes.com>
2020-06-04 17:47:32 +0300

    update Main in task_4

--- implementation/task_4/sources/src/Main.java
+++ implementation/task_4/sources/src/Main.java
@@ -8,9 +8,37 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<Edge> edges = GraphFileParser.getEdgesFromFile("/home/merlin/study/multiprocessing/concurrency2k20/implementation/task_4/sources/src/graph.txt");
+        Integer mode = null;
+        String path_to_graph_data = null;
+        Integer source = null;
+        Integer destination = null;
+
+        if (args.length != 4) {
+            System.err.println("`mode` [1 for sequential, 2 for concurrent] " +
+                    "`path/to/graph/data.txt` `source` `destination`");
+            System.exit(1);
+        } else {
+            mode = Integer.parseInt(args[0]);
+            path_to_graph_data = args[1];
+            source = Integer.parseInt(args[2]);
+            destination = Integer.parseInt(args[3]);
+        }
+
+        List<Edge> edges = GraphFileParser.getEdgesFromFile(path_to_graph_data);
         var graph = new Graph(edges);
-        var djikstra = new DjikstraPathFinder();
-        var path = djikstra.getShortestPath(graph, 0, 1);
+        List<Integer> path = null;
+        if (mode == 0) {
+            var djikstra = new DjikstraPathFinder();
+            path = djikstra.getShortestPath(graph, source, destination);
+        } else if (mode == 1) {
+            System.out.println("Nothing to do here");
+        } else {
+            System.err.println("Incorrect `mode` value. It must be `1` for sequential or `2` for concurrent");
+            System.exit(1);
+        }
+        if (path == null) {
+            System.err.println("Can`t build path for such source and destination.");
+            System.exit(1);
+        }
     }
 }
