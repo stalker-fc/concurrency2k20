ebeab650ee03803ba411743261fa3f3c429747b4
Sergey Stepanov <stepanov.s@numdes.com>
2020-06-04 13:10:29 +0300

    update task_3 main

--- implementation/task_3/sources/main.cpp
+++ implementation/task_3/sources/main.cpp
@@ -15,19 +15,16 @@ int main(int argc, char *argv[]) {
     struct Array array = get_array(argv[1]);
     int mode = atoi(argv[2]);
 
-    bool is_correct;
     if (mode == 1) {
         quicksort(array.data, 0, array.length - 1);
-        is_correct = is_result_correct(array);
     } else if (mode == 2) {
         init_MPI(argc, argv);
         quicksort_mpi(array.data, 0, array.length - 1);
-        is_correct = is_result_correct(array);
     } else {
         std::cerr << "Incorrect mode value. It must be 1 or 2." << std::endl;
         std::exit(1);
     }
-
+    bool is_correct = is_result_correct(array);
     if (!is_correct) {
         std::cerr << "Array has sorted incorrectly." << std::endl;
         std::exit(1);
